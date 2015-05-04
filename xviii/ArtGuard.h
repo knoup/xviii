59a2e2b6d1bfd82fccea0336858ed0746e0d61d0
Ralph Chaanine <10029584+ralphchaanine@users.noreply.github.com>
2015-05-04 15:25:52 +0300

    forgot to add these 2 commits ago

---
+++ xviii/ArtGuard.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "Infantry.h"
+
+class ArtGuard : public Infantry
+{
+public:
+	ArtGuard(World& _world, std::mt19937& _mt19937, Player* _belongsToPlayer, TextureManager& tm, FontManager& fm, UnitTile::Direction _dir, TextureManager::Unit texType = TextureManager::Unit::ARTGUARD, UnitType uType = UnitType::ARTGUARD);
+
+	inline virtual int getMaxHp() const{ return maxhp; };
+	inline virtual int getMaxMov() const{ return maxMov; };
+	inline virtual int getMaxRange() const{ return maxRange; };
+
+	virtual std::string rangedAttack(UnitTile* unit, int distance);
+
+private:
+	static const int maxhp{4};
+	static const int maxMov{3};
+	static const int maxRange{4};
+};
+
