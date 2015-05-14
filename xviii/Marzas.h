6482779e8c66a5a03cbc995b277ada6e099b17a9
Ralph Chaanine <10029584+ralphchaanine@users.noreply.github.com>
2015-05-14 23:54:55 +0300

    forgot to add these, again

---
+++ xviii/Marzas.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "Cavalry.h"
+
+class Marzas : public Cavalry
+{
+public:
+	Marzas(World& _world, std::mt19937& _mt19937, Player* _belongsToPlayer, TextureManager& tm, FontManager& fm, UnitTile::Direction _dir);
+
+	//Skirmisher
+	virtual bool canSkirmish(){ return true; };
+
+	inline virtual int getMaxHp() const{ return maxhp; };
+	inline virtual int getMaxMov() const{ return maxMov; };
+	inline virtual int getMaxRange() const{ return maxRange; };
+
+	virtual std::string rangedAttack(UnitTile* unit, int distance);
+
+private:
+	static const int maxhp{10};
+	static const int maxMov{12};
+	static const int maxRange{8};
+};
+
