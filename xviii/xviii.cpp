c77a3108dcba4dd5251fb6fc830b4ad3fa102dd1
Ralph Chaanine <10029584+ralphchaanine@users.noreply.github.com>
2015-06-15 12:57:03 +0300

    specify subsystem with linker flags instead of this

--- xviii/xviii.cpp
+++ xviii/xviii.cpp
@@ -1,8 +1,6 @@
 #include "stdafx.h"
 #include "Game.h"
 
-#include <Windows.h>
-
 /*
 TODO:
 	-Have a basic readable config file where several simple parameters can be set (resolution, deployment points,
@@ -16,17 +14,7 @@ IMPORTANT:
 If you are using a custom tile set, make sure tile sizes are set correctly in TextureManager
 */
 
-//Release version
 
-/*
-int CALLBACK WinMain(_In_  HINSTANCE hInstance, _In_  HINSTANCE hPrevInstance, _In_  LPSTR lpCmdLine, _In_  int nCmdShow)
-{
-	Game game;	
-	return 0;
-}	
-*/
-
-//Debug version:
 int main(int argc, char* argv[]){
 	Game game;
 	return 0;
