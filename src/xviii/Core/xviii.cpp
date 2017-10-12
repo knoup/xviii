c2f63a1c51d5012de94368b102d22d7ea881cb5c
Ralph Chaanine <10029584+ralphchaanine@users.noreply.github.com>
2017-10-12 15:10:39 +0300

    todo list changes

--- src/xviii/Core/xviii.cpp
+++ src/xviii/Core/xviii.cpp
@@ -4,25 +4,38 @@
 /*
     TODO:
 
-    (*) Make some of the rain logic in GameState_Play::update() be updated turnly instead of constantly
+    ////////////////////////////////////////////////////////////////////////
+                                [PRIORITY - high]
+    ////////////////////////////////////////////////////////////////////////
 
-    (*) Seperate movement and attack logic so that classes can, for example, be given movement rules of CAV but
-    attack rules of INF (TODO later, after the upcoming UI update)
+        (*) Improve UI
 
-    (*) At some point, perhaps create a function that returns every tile in between 2 given tiles, for less code
-    repetition in places (UnitTile::distanceFrom(), UnitTile::moveTo(), UnitTile::rangedAttack()...). At the moment
-    there are some complexities (and laziness) which prevent me from creating a unified solution, but it's something
-    to look into sometime.
+        (*) Allow terrain tiles to affect movement. Note that this should work even for terrain tiles in between
+            the current and destination tiles, not just tiles manually moved on.
 
-    (*) Implement a specialised HillAnt class (for slopes and hills)
+    ////////////////////////////////////////////////////////////////////////
+                                [PRIORITY - medium]
+    ////////////////////////////////////////////////////////////////////////
 
-	(*) Have a basic readable config file where several simple parameters can be set (resolution, deployment points, etc.)
+        (*) Implement a specialised HillAnt class (for slopes and hills)
 
-	(*) Make the presence of mud be dependent on rainy weather conditions
+        (*) Have a basic readable config file where several simple parameters can be set (resolution, deployment points, etc.)
 
-	(*) Make rivers have a chance to bend, perhaps by giving each tile a minor chance to do a 90 degree turn
+        (*) Make rivers have a chance to bend, perhaps by giving each tile a minor chance to do a 90 degree turn
 
-	(*) UI Update
+    ////////////////////////////////////////////////////////////////////////
+                                [PRIORITY - low/none]
+    ////////////////////////////////////////////////////////////////////////
+
+        (*) Make some of the rain logic in GameState_Play::update() be updated turnly instead of constantly
+
+        (*) Seperate movement and attack logic so that classes can, for example, be given movement rules of CAV but
+        attack rules of INF (TODO later, after the upcoming UI update)
+
+        (*) At some point, perhaps create a function that returns every tile in between 2 given tiles, for less code
+        repetition in places (UnitTile::distanceFrom(), UnitTile::moveTo(), UnitTile::rangedAttack()...). At the moment
+        there are some complexities (and laziness) which prevent me from creating a unified solution, but it's something
+        to look into sometime.
 
 */
 
